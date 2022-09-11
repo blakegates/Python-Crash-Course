03f3cc77a77b3d5f0928d6ced73b1b4e561eeb3b
blakegates <blakegatesllc@gmail.com>
2022-09-11 09:26:12 -0400

    rev

--- Chapter 4/one_million.py
+++ Chapter 4/one_million.py
@@ -1,3 +1,6 @@
-numbers = []
-for numbers in range(1,1000001):
-    print(numbers)
+#numbers = []
+#for numbers in range(1,1000001):
+print(sum(range(1, 1000001)))
+
+#million = [sum(numbers in range(1,1000001))]
+#print(million)
