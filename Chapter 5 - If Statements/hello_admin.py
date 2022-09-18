4304714af082398c5820d402fbf59a32191fc87a
blakegates <blakegatesllc@gmail.com>
2022-09-18 12:10:26 -0400

    ch 5

---
+++ Chapter 5 - If Statements/hello_admin.py
@@ -0,0 +1,7 @@
+users = ['admin', 'belle', 'bri', 'blake', 'jack', 'mosh']
+
+for user in users:
+    if user == 'admin':
+        print(f"Hello admin, would you like to view the system reports?")
+    else:
+        print(f"Hello {user}.")
